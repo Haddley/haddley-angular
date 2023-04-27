89d2cb008d4289a91bb1cee704dea628ea3f0a82
neilhaddley <neil@haddley.com>
2023-04-27 11:04:00 -0500

    Private message storage

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { Firestore, addDoc, collection, collectionData, getFirestore, query, serverTimestamp, where } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,11 +12,42 @@ import { Auth } from '@angular/fire/auth';
 export class DashboardComponent implements OnInit {
 
   user = this.auth.currentUser;
+  createMessageForm!: FormGroup;
+  messages$: Observable<any[]>;
 
-  constructor(private auth: Auth) { }
+  constructor(private formBuilder: FormBuilder, private auth: Auth, firestore: Firestore) {
+    this.createMessageForm = this.formBuilder.group({
+      message: formBuilder.control('', [Validators.required, Validators.minLength(6)])
+    })
+    const uid = this.auth.currentUser?.uid;
+    const result = query(collection(firestore, 'messages'), where("uid", "==", uid));
+    console.log(result);
+    this.messages$ = collectionData(result);
+  }
 
   ngOnInit(): void {
     console.log(this.auth.currentUser);
+  }
+
+  public onSubmit(): void {
+    this.saveMessage(this.createMessageForm.value.message)
+    this.createMessageForm.reset();
+  }
+
+  // Saves a new message to Cloud Firestore.
+  private async saveMessage(messageText: string) {
+    try {
+      const uid = this.auth.currentUser?.uid;
+      await addDoc(collection(getFirestore(), 'messages'), {
+        uid: uid,
+        email: this.auth.currentUser?.email,
+        text: messageText,
+        timestamp: serverTimestamp()
+      });
+    }
+    catch (error) {
+      console.error('Error writing new message to Firebase Database', error);
+    }
   }
 
 }
